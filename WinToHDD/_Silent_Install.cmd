3609e909f399cd143d49367958f70021cd1eb372
Tools Setup Windows <riovtg@gmail.com>
2025-09-23 12:07:21 +0700

    Update _Silent_Install.cmd

--- WinToHDD/_Silent_Install.cmd
+++ WinToHDD/_Silent_Install.cmd
@@ -1,6 +1,21 @@
-@echo off
-title Cai dat WinToHDD
-FOR %%i IN ("WinToHDD*.exe") DO Set FileName="%%i"
-%FileName% /SILENT
+if exist "C:\Program Files\Hasleo\WinToHDD\bin\WinToHDD.exe" (
+    echo Da ton tai WinToHDD. Bo qua cai dat.
+    endlocal
+    exit /b 0
+)
+
+:: --- Tìm file cài đặt khớp WinToHDD*.exe ---
+set "FileName="
+for %%i in ("WinToHDD*.exe") do (
+    set "FileName=%%~fi"
+    goto :run
+)
+
+echo Khong tim thay file cai dat: WinToHDD*.exe
+endlocal
+exit /b 1
+
+:run
+"%FileName%" /SILENT
 endlocal
 exit /b
